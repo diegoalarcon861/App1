4bdbb11b7844c193de671334da2a69d86430a747
diego <diegoalarcon030609@gmail.com>
2026-05-09 21:27:32 -0600

    Initial commit

---
+++ app/src/main/java/com/example/application/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.example.application.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Background = Color(0xFFF8F8F8)
+val Terracota = Color(0XFFE76F51)
+val NegroSuave = Color(0xFF1A1A1A)
+val Mostaza = Color(0xFFE9C46A)
+val WarmSand = Color(0xFFC9B99A)
+val TextSec = Color(0xFF6B6358)
+val NegroBase = Color(0xFF0E0E0E)
+val GrisOsc = Color(0xFF262422)
